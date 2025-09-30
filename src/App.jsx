84f0dbb2ab523e51abbc26c78af697d18e264fe1
Ahmad <ahmad.s.bugshan@gmail.com>
2025-09-30 16:59:23 +0300

    Added dies buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
-export default function App(){
-    return( 
+import Die from "./Die"
+
+export default function App() {
+    return (
         <main>
-            <div></div>
+            <div className="dice-container">
+                <Die value={1} />
+                <Die value={5} />
+                <Die value={2} />
+                <Die value={6} />
+                <Die value={4} />
+                <Die value={3} />
+                <Die value={2} />
+                <Die value={6} />
+                <Die value={1} />
+                <Die value={4} />
+            </div>
         </main>
     )
 }
